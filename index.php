77a9df7da3bcd68e142e535af26af3f3e308ed34
Dmitry Medelyaev <dimanych90@gmail.com>
2020-02-22 22:58:43 +0300

    Добавил задание 6 где таблица с умножениями

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 $name = 'Dimasic';       // задание №1
 $age = 29;
 
-echo 'Меня зовут'. ' '. $name .'<br>'. 'Мне'. ' '. $age . ' ' . 'лет.';
+echo 'Меня зовут' . ' ' . $name . '<br>' . 'Мне' . ' ' . $age . ' ' . 'лет.';
 
 echo '<hr>';
 echo '<br>';
@@ -33,13 +33,13 @@ echo '<br>';
 
 // задание №3
 
-$age= 34;
+$age = 34;
 if ($age >= 18 && $age <= 65
 )
     echo "Вам еще работать и работать";
 elseif ($age > 65)
     echo "Вам пора на пенсию";
-elseif ($age <= 17 && $age >= 1)
+elseif ($age < 17 && $age >= 1)
     echo "Вам пока рано работать";
 else
     echo 'Неизвестный возраст';
@@ -79,37 +79,67 @@ echo '<hr>';
 echo '<br>';
 
 // задание №5
+
 $bmw = [
     'model' => 'X5',
     'speed' => 120,
-    'doors' => 5 ,
+    'doors' => 5,
     'years' => 2015
 ];
 $tayota = [
     'model' => 'camry',
     'speed' => 150,
-    'doors' => 4 ,
+    'doors' => 4,
     'years' => 2015
 ];
 $opel = [
     'model' => 'insignia',
     'speed' => 139,
-    'doors' => 4 ,
+    'doors' => 4,
     'years' => 2016
 ];
 
-$cars = array_merge($bmw,$tayota,$opel);  //не могу понять что не так, выводит только опель
+$cars = ['bmw' => $bmw,
+    'tayota' => $tayota,
+    'opel' => $opel
+];
 
-echo '<pre>';
-print_r($cars);
+
+foreach ($cars as $key => $value) {
+    echo 'Car ' . $key . '<br>';
+    $result = implode(' ', $value);
+    echo $result . '<br>';
+}
 
 echo '<hr>';
 echo '<br>';
 
 // задание №6
+$rows = 10; // количество строк, tr
+$cols = 10; // количество столбцов, td
 
-for ('<table width="10" height="10">'; );
+echo '<table border="1">';
 
 
+for ($tr = 1; $tr <= $rows; $tr++) {
+
+    echo '<tr><td>'. $tr .'</td>';
+    for ($td = 1; $td <= $cols; $td++) {
+        if ($tr > 1) {
+            $result = $td * $tr;
+            if ($result % 2 === 0) {
+                echo '<td>(' . $result . ')</td>';
+            } elseif ($result % 2 !== 0) {
+                echo '<td>[' . $result . ']</td>';
+            } else
+                echo '<td>' . $result . '</td>';
+        }else{
+            echo '<td>' . $td . '</td>';
+        }
+    }
+    echo '</tr>';
+}
+
+echo '</table>';
 
 ?>
